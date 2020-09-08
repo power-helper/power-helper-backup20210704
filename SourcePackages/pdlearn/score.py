98252023836a20dfc1404b992596da085474fe45
panweil <32158677+panweil@users.noreply.github.com>
2020-09-08 22:45:40 +0800

    Update score.py

--- SourcePackages/pdlearn/score.py
+++ SourcePackages/pdlearn/score.py
@@ -9,13 +9,13 @@ def get_score(cookies):
         for cookie in cookies:
             jar.set(cookie['name'], cookie['value'])
         total = requests.get("https://pc-api.xuexi.cn/open/api/score/get", cookies=jar,
-                             headers={'Cache-Control': 'no-cache'}).content.decode("utf8")
+                            headers={'Cache-Control': 'no-cache'}).content.decode("utf8")
         total = int(json.loads(total, encoding="utf8")["data"]["score"])
         each = requests.get("https://pc-api.xuexi.cn/open/api/score/today/queryrate", cookies=jar,
                             headers={'Cache-Control': 'no-cache'}).content.decode(
             "utf8")
         each = json.loads(each, encoding="utf8")["data"]["dayScoreDtos"]
-        each = [int(i["currentScore"]) for i in each if i["ruleId"] in [1, 2, 9, 1002, 1003,6, 5, 4]]
+        each = [int(i["currentScore"]) for i in each if i["ruleId"] in [1, 2, 9, 1002, 1003, 6, 5, 4]]
         return total, each
     except:
         print("=" * 120)
