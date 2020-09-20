da90eeebd5df8b7d3a4c9595fb90ba59238276df
保尔，柯察金 <admin@xuexi.cn>
2020-09-20 20:10:57 +0800

    说明

--- SourcePackages/pandalearning.py
+++ SourcePackages/pandalearning.py
@@ -575,14 +575,23 @@ if __name__ == '__main__':
     #  0 读取版本信息
     start_time = time.time()
 
-    print('''现支持以下模式：
+    print('''
+    科技强国官方网站：https://techxuexi.js.org
+    Github地址：https://github.com/TechXueXi
+使用本项目，必须接受以下内容，否则请立即退出：
+    - TechXueXi 仅额外提供给“热爱党国”且“工作学业繁重”的人
+    - 项目开源协议 LGPL-3.0
+    - 不得利用本项目盈利
+另外，我们建议你参与一个维护劳动法的项目：
+https://996.icu/ 或 https://github.com/996icu/996.ICU/blob/master/README_CN.md
+TechXueXi 现支持以下模式（答题时请值守电脑旁处理少部分不正常的题目）：
     1 文章+视频
     2 每日答题+每周答题+专项答题+文章+视频
       （可以根据当日已得做题积分，及是否有可得分套题，决定是否做题）
     3 每日答题+文章+视频
       （可以根据当日已得做题积分，决定是否做题）
       ''')
-    TechXueXi_mode = input("请选择模式并回车： ")
+    TechXueXi_mode = input("请选择模式（输入对应数字）并回车： ")
 
     info_shread = threads.MyThread("获取更新信息...", version.up_info)
     info_shread.start()
